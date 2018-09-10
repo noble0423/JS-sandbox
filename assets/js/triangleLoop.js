8e07db7db2f5ac820617349854164c0d0795b04d
noble0423 <ryan.noble82@gmail.com>
2018-09-10 15:02:36 -0500

    triangleLoop complete

--- assets/js/triangleLoop.js
+++ assets/js/triangleLoop.js
@@ -1,11 +1,23 @@
 // VARIABLES 
 //============================================================================================
-
+let line = "#";
 
 
 // FUNCTIONS
 //============================================================================================
+function triangleLoop() {
+    console.log(line);
+    for (i = 0; i <= 10; i++) {
+        console.log(line += "#");
+    }
+}
 
+// Simplified code
+function triangleLoop2() {
+    for (let line = "*"; line.length <= 10; line += "*") {
+        console.log(line);
+    }
+}
 
 
 // MAIN PROCESS
@@ -13,6 +25,8 @@
 
 $(document).ready(function() {
     $("#test-button").on("click", function() {
-        console.log("clicked");
+        // console.log("clicked");
+        triangleLoop();
+        triangleLoop2();
     });
 });
