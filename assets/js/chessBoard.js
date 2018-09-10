c719d7994d69fa087e92cd46ce26da5680f13c6b
noble0423 <ryan.noble82@gmail.com>
2018-09-10 18:29:51 -0500

    chessboard taking user input and generating game board size correctly in the console.

--- assets/js/chessBoard.js
+++ assets/js/chessBoard.js
@@ -1,17 +1,25 @@
 // VARIABLES 
 //============================================================================================
-let height;
-let width;
+let board = "";
 
 
 // FUNCTIONS
 //============================================================================================
 function chessBoard() {
     console.log("---------------");
-    for (i = 1; i <= 4; i++) {
-        console.log("# # # # ");
-        console.log(" # # # #");
-    };
+    for (x = 1; x <= $("#boardWidth").val().trim(); x++) {
+        for (y = 1; y <= $("#boardHeight").val().trim(); y++) {
+            if ((x + y) % 2 === 0 ) {
+                board += " ";
+            }
+
+            else {
+                board += "#";
+            }
+        }
+        board += "\n";
+    }
+    console.log(board);
     console.log("---------------");
 };
 
@@ -22,9 +30,6 @@ $(document).ready(function() {
     $("#target").on("submit", function() {
         // console.log("clicked");
         event.preventDefault();
-        $("#target.height").val();
-        console.log("height " + height);
-        console.log("width " + width);
-        // chessBoard();
+        chessBoard();
     });
 });
