3d405022f80e0c9a7db1ef802ab5ac7b6f7a93da
noble0423 <ryan.noble82@gmail.com>
2018-09-12 11:47:16 -0500

    grabbing form data correctly, passing it into function, and evaluating correctly.

---
+++ assets/js/minimum.js
@@ -0,0 +1,32 @@
+// VARIABLES 
+//============================================================================================
+
+
+
+// FUNCTIONS
+//============================================================================================
+// Takes in 2 arguments and returns the minimum
+function minimum (x, y) {
+    // let x = $("#num1").val().trim();
+    // let y = $("#num2").val().trim();
+
+    if (parseInt(x) > parseInt(y)) {
+        console.log(y + " is the smaller value.");
+    }
+    else if (parseInt(x) === parseInt(y)) {
+        console.log(x + " and " + y + " are the value -- neither are smaller.");
+    }
+    else {
+        console.log(x + " is the smaller value.");
+    }
+};
+
+
+// MAIN PROCESS
+//============================================================================================
+$(document).ready(function() {
+    $("#target-minimum").on("submit", function() {
+        // console.log("clicked");
+        minimum($("#num1").val(), $("#num2").val());
+    });
+});
