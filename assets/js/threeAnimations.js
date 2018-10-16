d894b9eb8f2cb9fe3d33592c1898b149917396de
noble0423 <ryan.noble82@gmail.com>
2018-10-16 14:24:16 -0500

    switched three.js animation to different code (3D cube) and it is correctly displaying and animating on HTML.

---
+++ assets/js/threeAnimations.js
@@ -0,0 +1,49 @@
+var THREE = require('three');
+
+// var scene = new THREE.Scene();
+
+var scene3d = document.getElementById("scene3d");
+// var CANVAS_WIDTH = 300;
+// var CANVAS_HEIGHT = 300;
+
+var camera, scene, renderer;
+var geometry, material, mesh;
+
+init();
+animate();
+
+function init() {
+
+	camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
+	camera.position.z = 1;
+
+	scene = new THREE.Scene();
+
+	geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+	material = new THREE.MeshNormalMaterial();
+
+	mesh = new THREE.Mesh( geometry, material );
+	scene.add( mesh );
+
+	renderer = new THREE.WebGLRenderer( { antialias: true } );
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	document.body.appendChild( renderer.domElement );
+
+}
+
+function animate() {
+
+	requestAnimationFrame( animate );
+
+	mesh.rotation.x += 0.01;
+	mesh.rotation.y += 0.02;
+
+	renderer.render( scene, camera );
+
+}
+
+// FINISH SCENE SETUP
+
+// document.body.appendChild(scene3d.domElement);
+scene3d.appendChild(renderer.domElement);
+renderer.render(scene, camera);
