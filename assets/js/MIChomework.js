ad6e5527feadfa24b5967bfa465c04a05d066577
noble0423 <ryan.noble82@gmail.com>
2018-11-30 09:30:37 -0600

    MIC homework...pushing to new array.

--- assets/js/MIChomework.js
+++ assets/js/MIChomework.js
@@ -3,7 +3,7 @@ var deviceTimestamp = {
     T: 1542315590578.1729, 
     DT: 0.25, 
 	DATA: {
-		SIGNAL_1:[1, 2, 3, 4, 5, 6, 7, 8, 9],
+		SIGNAL_1: [1, 2, 3, 4, 5, 6, 7, 8, 9],
 		SIGNAL_2: [1,3,4] 
 	}
 }
@@ -22,7 +22,7 @@ function timestampIntervalCounter() {
 // loop thru signal_1 array
 for (let i = 0; i < deviceTimestamp.DATA.SIGNAL_1.length; i++) {
     // push signal and timestamp to new array
-    signalPtTimingArr.push([deviceTimestamp.DATA.SIGNAL_1[i]], deviceTimestamp.T);
+    signalPtTimingArr.push([[deviceTimestamp.DATA.SIGNAL_1[i]], deviceTimestamp.T]);
     // run timestamp interval function in order to increase the timestamp for ea index in the array
     timestampIntervalCounter();
 }
