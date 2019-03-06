f9857812cd322c90a5d94f5227bad20ba015631d
noble0423 <ryan.noble82@gmail.com>
2019-03-06 12:38:47 -0600

    completed all activities through Array-Logs--all work as designed.

--- TA-ActivityPractice/assets/js/app.js
+++ TA-ActivityPractice/assets/js/app.js
@@ -1,1 +1,121 @@
 // alert("JS pg is linked");
+
+// Pizza Variables Activity
+    // var name = "Ryan";
+    // var pizzaCount = 7;
+    // var isHungry = true;
+    // var totalCost = pizzaCount * 5.00;
+
+    // alert("Welcome: " + name);
+
+    // alert("Pizzas cost $5 each");
+
+    // alert("Your total is: $" + totalCost);
+
+    // alert("Still hungry: " + isHungry);
+
+// Pizza Variables Activity 2
+    // var name = "John Frasier";
+    // var pizzaCount = 15;
+    // var isHungry = true;
+    // var totalCost = pizzaCount * 5.00;
+
+    // console.log("Welcome: " + name);
+    // console.log("Pizzas cost $5 each.");
+    // console.log("Your total is: $" + totalCost);
+    // console.log("Still hungry: " + isHungry);
+
+// Prompt Demo 
+    // alert("We definitely rock!");
+
+    // var doYouRock = confirm("The question is, do *you* rock?");
+    // var howMuchRock = prompt("How much do you rock?");
+
+    // alert(doYouRock);
+    // alert(howMuchRock);
+
+// Prompt-Sushi
+    // var likeSushi = confirm("Do you like Sushi?");
+    // var sushiType = prompt("What kind of Sushi do you like?");
+
+    // alert("RE the question: 'Do you like Sushi?', you answered " + likeSushi);
+    // alert(sushiType + " is your favorite type of Sushi.");
+
+// Conditional Demo
+    // var confirmSushi = confirm("Do you like Sushi?");
+    // var confirmGingerTea = confirm("Do you like Ginger Tea?");
+    // var sushiType = prompt("What is your favorite type of Sushi?");
+
+    // if (confirmSushi && confirmGingerTea) {
+    //     alert("You like " + sushiType + " and Ginger Tea!");
+    // }
+    
+    // else if (confirmSushi) {
+    //     alert("You like " + sushiType + "!");
+    // }
+
+    // else if (confirmGingerTea) {
+    //     alert("You like Ginger Tea!");
+    // }
+
+    // else {
+    //     document.write("You don't like Sushi or Ginger Tea.");
+    // }
+
+// Conditional Activity
+    // var confirmSteak = confirm("Do you like steak?");
+
+    // if (confirmSteak) {
+    //     document.write("Here's a steak sandwich.");
+    // }
+
+    // else {
+    //     document.write("Here's a tofu stir-fry!");
+    // }
+
+    // var age = prompt("What year were you born?");
+
+    // if (parseInt(age) < 1998) {
+    //     alert("Sake Bombs away!");
+    // }
+
+    // else if (parseInt(age) === 1998) {
+    //     alert("I'm going to need to see some ID.");
+    // }
+
+    // else {
+    //     alert("No sake for you!");
+    // }
+
+// Arrays Demo
+    // var zooAnimals = ["Zebra", "Rhino", "Giraffe", "Owl"];
+
+    // console.log(zooAnimals.length);
+    // console.log(zooAnimals[1]);
+    // console.log(zooAnimals[4]);
+
+// Bands Activity
+    // var favBandsAllLowerCase = ["qotsa", "foo fighters", "eodm", "the kills"];
+    // var userBand = prompt("What is your favorite band?");
+    // var userBandLowerCase = userBand.toLowerCase();
+
+    // if (favBandsAllLowerCase.indexOf(userBandLowerCase) === -1) {
+    //     alert("Oh, I've never heard of " + userBand + " before. I'll have to check them out.");
+    // }
+
+    // else {
+    //     alert("Yay! " + userBand + " is the shit!");
+    // }
+
+// Array-logs Activity
+    // var coolPeople = ["bruce lee", "arnold schwarzenegger", "will smith", "esther", "marie curie", "joan of arc"];
+
+    // console.log(coolPeople[0]);
+    // console.log(coolPeople[1]);
+    // console.log(coolPeople[2]);
+    // console.log(coolPeople[3]);
+    // console.log(coolPeople[4]);
+    // console.log(coolPeople[5]);
+
+// 
+    
