a41c29a77179f88792f0becdd93cea85a0a45c13
noble0423 <ryan.noble82@gmail.com>
2019-03-21 16:01:46 -0500

    simpleTimer activity is complete...although there is a bug RE audio.play(), html5, and apparently chrome. Since the audio playing is not a concern bc the timeUp function still works, I am moving on.

--- TA-ActivityPractice/Timers/assets/js/simpleTimer.js
+++ TA-ActivityPractice/Timers/assets/js/simpleTimer.js
@@ -1,1 +1,38 @@
-alert("linked");
+var audio = new Audio("./assets/audio/raven.mp3");
+
+
+const fiveSeconds = function() {
+    $("#time-left").html("<h2>About 10 seconds left!</h2>");
+    console.log("10 secs left");
+}
+
+const tenSeconds = function() {
+    $("#time-left").html("<h2>About 5 seconds left!</h2>");
+    console.log("5 secs left");
+}
+
+const timeUp = function() {
+    $("#time-left").html("<h2>Time's Up!</h2>");
+    console.log("Time's Up!");
+    audio.play();
+}
+
+setTimeout(fiveSeconds, 1000 * 5);
+setTimeout(tenSeconds, 1000 * 10);
+setTimeout(timeUp, 1000 * 15);
+
+// function fiveSeconds() {
+//     $("#time-left").html("<h2>About 10 seconds left!</h2>");
+//     console.log("10 secs left");
+// }
+
+// function tenSeconds() {
+//     $("#time-left").html("<h2>About 5 seconds left!</h2>");
+//     console.log("5 secs left");
+// }
+
+// function timeUp() {
+//     $("#time-left").html("<h2>Time's Up!</h2>");
+//     console.log("Time's Up!");
+//     audio.play();
+// }
